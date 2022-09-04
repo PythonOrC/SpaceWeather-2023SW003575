4660a675936bc4ff7fae75b586908191fc62ed0f
Ethan Hu <ethanhu8351@gmail.com>
2022-09-03 19:24:10 -0700

    changed to all levels

--- R Scripts/waveletExtract.r
+++ R Scripts/waveletExtract.r
@@ -1,10 +1,19 @@
+# TODO: interpolation
+# x <- 1:1440
+# FRD_wavelet_tmp <- approx(x, FRD_wavelet_tmpp, method = "linear", n = 1440)$y
+#                                   ^ original data
+
 library(waveslim)
 
 is.nan.data.frame <- function(x) {
     do.call(cbind, lapply(x, is.nan))
 }
 
-extract <- function(data, level) {
+toDataFrame <- function(station, time, data) {
+    return(data.frame(Station = c(station), Date_UTC = c(time), Wavelet = c("dBN"), Data = data[[station]]))
+}
+
+extract <- function(data) {
     # Extract the wavelet coefficients at a given level
     # data: the data to be processed
     # level: the level of the wavelet coefficients to be extracted
@@ -21,21 +30,20 @@ extract <- function(data, level) {
         single_data[is.nan(single_data)] <- 0
         data_wavelet.wt <- modwt(single_data, wf = wf, n.levels = levels, boundary = boundary)
         data_wavelet_mra <- mra.wt(data_wavelet.wt)
-
-        data_wavelet[[station]] <- data_wavelet_mra[[level]]
+        data_wavelet[[station]] <- data_wavelet_mra
     }
     return(data_wavelet)
 }
 
-write_to_csv <- function(data, file_name, level) {
+write_to_csv <- function(data, time, file_name) {
     if (file.exists(file_name)) {
         file.remove(file_name)
     }
-    col_names <- TRUE
     station <- names(data)[1]
-    content <- data.frame(Station = c(station), Wavelet = c("dBN"), Level = c(level), Data = data[[station]])
+    content <- toDataFrame(station, time, data)
+
     for (station in names(data)[-1]) {
-        content <- rbind(content, data.frame(Station = c(station), Wavelet = c("dBN"), Level = c(level), Data = data[[station]]))
+        content <- rbind(content, toDataFrame(station, time, data))
     }
     write.csv(content, file_name, row.names = FALSE)
 }
@@ -46,18 +54,12 @@ read_data <- function(file_name, argument) {
     return(data[, columns])
 }
 
-process_data <- function(input, output, argument, level) {
+process_data <- function(input, output, argument) {
     data <- read_data(input, argument)
-    data_wavelet <- extract(data, level)
-    write_to_csv(data_wavelet, output, level)
+    time <- unique(data$Date_UTC)
+    data_wavelet <- extract(data)
+    write_to_csv(data_wavelet, time, output)
 }
-
-
-
-
-
-
-
 
 # for (j in 1:levels) {
 #     plot(VIC_wavelet_mra_1029dbn[[j]], type = "l", , xlab = "", ylab = "nT")
@@ -73,5 +75,6 @@ process_data <- function(input, output, argument, level) {
 # BOU, NEW, BSL, FRD, FRN, TUC
 
 # process_data("20031029-supermag.csv", "VIC_dbn.csv", "dbn_nez", 1)
-process_data("20031029-supermag-6-stations.csv", "6_station_dbn.csv", "dbn_nez", 1)
+process_data("20031029-supermag-6-stations.csv", "6_station_dbn.csv", "dbn_nez")
+
 print("all Done")
