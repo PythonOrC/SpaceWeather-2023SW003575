a162ac7cedaad43eb26de88fe23dac7ed7d2c26e
Ethan Hu <ethanhu8351@gmail.com>
2022-11-05 20:01:04 -0700

    added:  - .shp file (map) to the image  - all 50 something stations to graph  - custom color bar to the graph
    fixed:
     - the value bar is now correct
     - the graph is now filled by the meshgrid

--- create_animation.py
+++ create_animation.py
@@ -2,7 +2,7 @@ import glob
 from PIL import Image
 def make_gif(frame_folder):
     frames = [Image.open(image) for image in glob.glob(f"{frame_folder}/*.png")]
-    frames[0].save(f"{frame_folder}.gif", format='GIF', append_images=frames[1:], save_all=True, duration=50, loop=0)
+    frames[0].save(f"{frame_folder}.gif", format='GIF', append_images=frames[1:], save_all=True, duration=75, loop=0)
 
 
 
--- matlab/examples/main_dbn_nez.m
+++ matlab/examples/main_dbn_nez.m
@@ -1,14 +1,23 @@
 DATA = 'dbn_nez';
-TIME = 309;
-DURATION = 240;
-
+TIME = 444;
+DURATION = 5;
+INTERVAL = TIME:TIME+DURATION;
+MARGIN = 3;
 % import the raw unprocessed data
-raw = readtable("supermag-6-stations.csv", "Delimiter",",", "DatetimeType","datetime");
+%raw = readtable("supermag-6-stations.csv", "Delimiter",",", "DatetimeType","datetime");
+raw = readtable("all-na-stations-noRAN+SMI+UPN.csv", "Delimiter",",", "DatetimeType","datetime");
 % get the stations from the raw data
 [Stations,IA,IC] = unique(raw.IAGA);
 % get the latitude and longitude of each station
 lat = raw.GEOLAT(1:length(Stations), 1);
 long = raw.GEOLON(1:length(Stations), 1);
+clear LOC;
+[LOC(1:length(Stations)).Geometry] = deal('Point');
+for i = 1:length(Stations)
+    LOC(i).Lat = lat(i);
+    LOC(i).Lon = long(i);
+end
+
 %convert the lat and long from [0,360] to [-180,180]
 for i = 1:length(lat)
     if lat(i) > 180
@@ -18,6 +27,41 @@ for i = 1:length(lat)
         long(i) = long(i) - 360;
     end
 end
+
+% max_lat = lat(1);
+% min_lat = lat(1);
+% max_long = lat(1);
+% min_long = lat(1);
+% min_long_index = 1;
+% max_long_index = 1;
+% min_lat_index = 1;
+% max_lat_index = 1;
+% for i = 1:length(lat)
+%     if lat(i) > max_lat
+%         max_lat = lat(i);
+%         max_lat_index = i;
+%     end
+%     if lat(i) < min_lat
+%         min_lat = lat(i);
+%         min_lat_index = i;
+%     end
+%     if long(i) > max_long
+%         max_long = long(i);
+%         max_long_index = i;
+%     end
+%     if long(i) < min_long
+%         min_long = long(i);
+%         min_long_index = i;
+%     end
+% end
+clear max;
+clear min;
+max_lat = max(lat) + MARGIN;
+min_lat = min(lat) - MARGIN;
+max_long = max(long) + MARGIN;
+min_long = min(long) - MARGIN;
+clear max;
+clear min;
 % extract the necessary data from the raw data
 clear data;
 data = {};
@@ -25,8 +69,7 @@ for i = 1:length(Stations)
     % raw datum refers to all the data from a single station
     raw_datum = raw(raw.IAGA == string(Stations(i)), :);
     % extract the needed datum from the raw datum
-    % datum = raw_datum.dbn_nez;
-    datum = table2array(raw_datum(:,{DATA}));
+    datum = table2array(raw_datum(INTERVAL,{DATA}));
     %interpolate the Nan values
     datum = fillmissing(datum, 'linear');
     % add the datum to the data cell array
@@ -55,22 +98,41 @@ end
 %get the upper and lower bounds of the data
 clear min;
 clear max;
+
 min = min(min(dat));
 max = max(max(dat));
     %Use meshgrid to create a set of 2-D grid points in the longitude-latitude plane and then use griddata to interpolate the corresponding depth at those points:
-[longi,lati] = meshgrid(-127:0.5:-66, 25:0.5:50); % * 0.5 is the resolution, longitude then latitude
+[longi,lati] = meshgrid(min_long:1:max_long, min_lat:1:max_lat); % * 0.5 is the resolution, longitude then latitude
+%[longi,lati] = meshgrid(-127:0.5:-66, 25:0.5:50);
 % graph the data
-for t = TIME:TIME+DURATION
+% for t = TIME:TIME+DURATION
+for t = 1:DURATION
+    disp("Generating...");
     dat_c = dat(t,:);
     v = variogram([OBS.long OBS.lat],dat_c');
     [~,~,~,vstruct] = variogramfit(v.distance,v.val,[],[],[],'model','stable');
     close;
     [OBSi,OBSVari] = krigingtest(vstruct,OBS.long',OBS.lat',dat_c,longi,lati);
+    
+    
+  figure('Color','w', 'Position',[0 0 1280 720]);
+
+    
+
+
     h=pcolor(longi,lati,OBSi); % * draw the points
+    hold on
     set(h,'EdgeColor','none'); 
+        s = shaperead('landareas.shp');
+    mapshow(LOC(1:length(LOC)),'Marker','o',...
+    'MarkerFaceColor','c','MarkerEdgeColor','k');
+    text(OBS.long,OBS.lat, [string(OBS.Stations)],'FontWeight','bold');
+    mapshow(s,'FaceAlpha', 0);
+    
+    % colormap gray;
     xlabel('Longitude'), ylabel('Latitude'), colorbar; 
     clim([min max]) % * colorbar range
-    str_title2=[DATA,' 6 stations 20031029 minute-',num2str(t)];
+    str_title2=[DATA,' 20031029 minute-',num2str(t+TIME)];
     title(str_title2);
     annotation('textbox',...
         [0.84 0.76 0.077 0.052],... % * position of the text box
@@ -79,9 +141,8 @@ for t = TIME:TIME+DURATION
         'FontName','Arial',...
         'FitBoxToText','off',...
         'LineStyle','none');
-    xlim([-127 -66]); % * longitude range
-    ylim([25 50]); % * latitude range
-    text(OBS.long,OBS.lat, [string(OBS.Stations)],'FontWeight','bold');
-    hold on
+    xlim([min_long max_long-1]); % * longitude range
+    ylim([min_lat max_lat-1]); % * latitude range
+    
     saveas(gcf,['D:\Github Repository\SpaceWeather\matlab\examples\draftFigure\',num2str(t),'.png'], 'png')
 end
